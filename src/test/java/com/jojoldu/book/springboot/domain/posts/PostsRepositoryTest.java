01b80e52913a17b2b579ddf5a7601f14297023e3
tbs03171 <tbs03171@naver.com>
2023-08-14 15:31:09 +0900

    [feat]

--- src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -11,7 +11,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -8,8 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
